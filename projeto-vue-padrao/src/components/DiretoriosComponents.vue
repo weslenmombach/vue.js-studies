956bd5b072d5b2365d698727e12c416ea8976b47
weslenmombach <weslensevero@hotmail.com>
2024-10-21 07:59:17 -0300

    Aula sobre pacotes, versionamentos e documentações

--- projeto-vue-padrao/src/components/DiretoriosComponents.vue
+++ projeto-vue-padrao/src/components/DiretoriosComponents.vue
@@ -3,6 +3,17 @@
 <template>
   <header>
     <h1>DIRETÓRIOS</h1>
+    <p>Criação de pastas, diretórios, configurações, estrutura de projetos e outros</p>
+    <hr />
+    <h2>Aula sobre package.json e package-lock.json.</h2>
+    <p>Caracteres e sets para versões localizados no package.json</p>
+    <p>package-lock.json é criado automaticamente e representa árvore de pacotes que fazem parte do projeto facilitando na hora do compartilhamento as infomrações com outras pessoas.</p>
+    <hr>
+    <p>node_modules normalmente não vai no compartihamento de projetos, pois já temos os json.</p>
+    <p>Os json são arquivos que guardar as configs do projeto e relações de dependências dos projetos</p>
+    <p>node_modules é onde é criado cada uma das dependências</p>
+    <p>.gitignore é onde listamos diretórios e arquivos que devem ser ignorados no processo de versionamento</p>
+    <p>README.md é basicamente pra descrever sobre o projeto, documentação, orientações e outros</p>
   </header>
 </template>
 
